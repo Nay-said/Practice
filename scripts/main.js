29553dbdcc2effc67c6d5db6c46d70649f1177f1
nay-said <colin-weber@outlook.com>
2019-05-24 16:23:47 +1200

    All donegit add -A!

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,10 @@
 var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]'; //Image
 var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]'; //Title
 var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]'; //URL
+var DETAIL_FRAME_SELECTOR = '[data-image-role="frame"]'; //Image for transitoin
+var HIDDEN_DETAIL_CLASS = 'hidden-detail'; //Hide big image
+var TINY_EFFECT_CLASS = 'is-tiny';
+var ESC_KEY = 27; //KeyCode of ESC
 
 //Change big image & title
 function setDetails(imageUrl, titleText) {
@@ -34,21 +38,54 @@ function setDetailsFromThumb(thumbnail) {
 function addThumbClickHandler(thumb) {
     thumb.addEventListener('click', function (event) {
         event.preventDefault();
-        setDetailsFromThumb(thumb); 
+        setDetailsFromThumb(thumb); //Call Back
+        showDetails();
     });
 }
 
+//Get all thumbnails
 function getThumbnailsArray(){
     'use strict';
     var thumbnails = document.querySelectorAll(THUMBNAIL_LINK_SELECTOR);
-    var thumbnailArray = [].slice.call(thumbnails);
+    var thumbnailArray = [].slice.call(thumbnails); //Convert to array
     return thumbnailArray;
 }
 
+//Hide big image. Add hidden-detail calss to body
+function hideDetails()  {
+    'use strict';
+    document.body.classList.add(HIDDEN_DETAIL_CLASS);
+}
+
+//Show big image
+function showDetails() {
+    'use strict';
+    var frame = document.querySelector(DETAIL_FRAME_SELECTOR);
+    document.body.classList.remove(HIDDEN_DETAIL_CLASS);
+    frame.classList.add(TINY_EFFECT_CLASS);
+    setTimeout(function () {
+        frame.classList.remove(TINY_EFFECT_CLASS);
+    }, 50);
+}
+
+//Key press event listener
+function addKeyPressHandler() {
+    'use strict';
+    document.body.addEventListener('keyup', function (event) {
+        event.preventDefault();
+        console.log(event.keyCode);
+        if (event.keyCode === ESC_KEY) {
+            hideDetails();
+        }
+    });
+}
+
+//Final step, initialize all processing logic
 function initializeEvents() {
     'use strict';
     var thumbnails = getThumbnailsArray();
-    thumbnails.forEach(addThumbClickHandler);
+    thumbnails.forEach(addThumbClickHandler); //Call back
+    addKeyPressHandler();
 }
 
 initializeEvents();
